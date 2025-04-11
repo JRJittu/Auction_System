092f2a4308beeeffa2a81686bfe87ffb04dad419
JRJittu <jrjittu.777@gmail.com>
2025-04-11 21:26:55 +0530

    initial commit

---
+++ Admin/src/constant.js
@@ -0,0 +1,5 @@
+export const BACKEND_PATH = "http://localhost:8080"
+export const USER_PATH = "http://localhost:8080/api/users"
+export const AUCTION_PATH = "http://localhost:8080/api/auctions"
+export const BID_PATH = "http://localhost:8080/api/bids"
+export const ADMIN_PATH = "http://localhost:8080/api/admin"
---
+++ Frontend/src/constant.js
@@ -0,0 +1,4 @@
+export const BACKEND_PATH = "http://localhost:8080"
+export const USER_PATH = "http://localhost:8080/api/users"
+export const AUCTION_PATH = "http://localhost:8080/api/auctions"
+export const BID_PATH = "http://localhost:8080/api/bids"
