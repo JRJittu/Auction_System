148605410ec31c4123d40fe569a28c0e409532af
JRJittu <jrjittu.777@gmail.com>
2025-04-12 00:10:00 +0530

    Log messaged added

--- Backend/src/main/java/com/AuctionSystem/AuctionSystemApplication.java
+++ Backend/src/main/java/com/AuctionSystem/AuctionSystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuctionSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Auction System Spring Boot App Started!");
+
 		SpringApplication.run(AuctionSystemApplication.class, args);
 	}
 
